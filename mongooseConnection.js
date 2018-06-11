cb79b15afeaa5efb06c5ab92892293e45f5cdcaa
federicobaiocco <baioccofede@gmail.com>
2018-06-10 21:17:34 -0300

    conexion a bdd mlab

--- mongooseConnection.js
+++ mongooseConnection.js
@@ -1,5 +1,5 @@
 'use strict';
 var mongoose = require("mongoose");
-var URL = "mongodb://localhost/SociedadSanJuan";
+var URL = "mongodb://frogsolutions:fedeputo123@@ds255320.mlab.com:55320/jesus-bdd";
 mongoose.connect(URL);
 module.exports = mongoose;
